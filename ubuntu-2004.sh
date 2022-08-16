fb7180f06b3d5f4977296ac33ea4fb3d0f92bc4b
tmowbray <tmowbray@dalabs.com>
2022-08-16 10:40:57 -0400

    fix apt-key add

--- ubuntu-2004.sh
+++ ubuntu-2004.sh
@@ -23,8 +23,8 @@ install_docker () {
 	apt install apt-transport-https ca-certificates curl software-properties-common -y
 
 	#PPA
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+	add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 
 	#INSTALL DOCKER
 	apt-get update
